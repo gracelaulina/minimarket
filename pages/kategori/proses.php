a98065a310582e03f75b2b35b0a9d20e2e485348
Ayu-Mutia <ayumutia183@gmail.com>
2025-06-12 15:06:48 +0700

    Update proses.php
    fiks

--- pages/kategori/proses.php
+++ pages/kategori/proses.php
@@ -1,13 +1,14 @@
 <?php
+// buat nyambungin ke database
 include("../../base_url.php");
 include('../../koneksi.php');
-
+// action bakal ngejalanin aksi sesuai action yg dituju
 if (isset($_POST["action"])) {
     $action = $_POST["action"];
 
-    if ($action == "edit" && isset($_POST["id_kategori"])) {
-        $id_kategori = $_POST["id_kategori"];
-        $query = mysqli_query($mysqli, "SELECT * FROM kategori WHERE ID_Kategori = $id_kategori");
+    if ($action == "edit" && isset($_POST["id_kategori"])) { // isset buat memastikan apakah ada atau tidak, $post mirip scanf
+        $id_kategori = $_POST["id_kategori"]; // $ sebagai variabel
+        $query = mysqli_query($mysqli, "SELECT * FROM kategori WHERE ID_Kategori = $id_kategori"); //mysqli_query buat jalanin query
         if ($data = mysqli_fetch_assoc($query)) {
             echo json_encode($data);
         } else {
@@ -16,17 +17,17 @@ if (isset($_POST["action"])) {
         exit;
     }
 
-    if ($action == "cek_kategori" && isset($_POST['nama_kategori'])) {
-        $nama_kategori = $_POST["nama_kategori"];
-        $query_cek = mysqli_query($mysqli, "SELECT * FROM kategori WHERE Nama_Kategori = '$nama_kategori'");
-        echo (mysqli_num_rows($query_cek) > 0) ? "ADA" : "GA ADA";
+    if ($action == "cek_kategori" && isset($_POST['nama_kategori'])) { // action saat cari kategori
+        $nama_kategori = $_POST["nama_kategori"]; //mendefinisikan variabel nama kategori
+        $query_cek = mysqli_query($mysqli, "SELECT * FROM kategori WHERE Nama_Kategori = '$nama_kategori'"); // query cari kategori berdasarkan nama kategori
+        echo (mysqli_num_rows($query_cek) > 0) ? "ADA" : "GA ADA"; //buat liat berapa baris sih haril run query, kalo > 0 ada, selain itu ga ada
         exit;
     }
 
     if ($action == "tambah_data" && isset($_POST['nama_kategori'])) {
         $nama_kategori = $_POST["nama_kategori"];
-        $query = mysqli_query($mysqli, "INSERT INTO kategori (Nama_Kategori) VALUES ('$nama_kategori')");
-        if ($query) {
+        $query = mysqli_query($mysqli, "INSERT INTO kategori (Nama_Kategori) VALUES ('$nama_kategori')"); // buat nambah data ke tabel
+        if ($query) { //buat cek data masuk ato engga
             echo json_encode(['success' => 'Data berhasil ditambahkan']);
         } else {
             echo json_encode(['error' => 'Data gagal ditambahkan']);
